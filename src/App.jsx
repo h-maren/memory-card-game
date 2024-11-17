8209a6bd40f0ce79743c61bc07df2792db0815ef
h-maren <26232423+h-maren@users.noreply.github.com>
2024-11-16 19:47:35 -0500

    Styled pokemon cards and added header

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,33 @@ function App() {
 
   return (
     <>
+    <header>
+    <h1>Pokemon Memory Game</h1>
+    <div className="score-header">
+      <h4>Game score: 0</h4>
+      <h4>Best score: 0</h4>
+    </div>
+    </header>
+    <div className="pokemonCardWrapper">
       <div>
       <GetPokemonInfo name={'pikachu'}></GetPokemonInfo>
       </div>
+      <div>
+      <GetPokemonInfo name={'charizard'}></GetPokemonInfo>
+      </div>
+      <div>
+      <GetPokemonInfo name={'ivysaur'}></GetPokemonInfo>
+      </div>
+      <div>
+      <GetPokemonInfo name={'squirtle'}></GetPokemonInfo>
+      </div>
+      <div>
+      <GetPokemonInfo name={'metapod'}></GetPokemonInfo>
+      </div>
+      <div>
+      <GetPokemonInfo name={'rattata'}></GetPokemonInfo>
+      </div>
+    </div>
     </>
   )
 }
--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import '../styles/PokemonCards.css'
 
 export default function GetPokemonInfo({name}){
     let [pokemon,setPokemon] = useState([]);
@@ -13,14 +14,19 @@ export default function GetPokemonInfo({name}){
             setLoading(false);
         });
     },)
-    let pokemonImage=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`+pokemon.id+`.png`;          
+    //let pokemonImage=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`+pokemon.id+`.png`;          
+    let pokemonImage = `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/0`+pokemon.id+`.png`;
+
+    if(pokemon.id<10){
+        pokemonImage= `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/00`+pokemon.id+`.png`;
+    }
 
     if(!isLoading){
     return(
         <div className='pokemonCard'>
-            <h2>{pokemon.name}</h2>
-            <h3>{pokemon.id}</h3>
+            <h2>{pokemon.name.toUpperCase()}</h2>
             <img src={pokemonImage} />
+            <h3>#{pokemon.id}</h3>
             <h3>{pokemon.types[0].type.name}</h3>
         </div>
     )
